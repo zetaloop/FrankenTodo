d7fd71457d180364d1c1b356d7eb0ec8596cfe40
zetaloop <zetaloop@outlook.com>
2024-12-25 16:20:31 +0800

    [frontend] Export to backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  distDir: '../backend/src/main/resources/static',
+  basePath: '',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
