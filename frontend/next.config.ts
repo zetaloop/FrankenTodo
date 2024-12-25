bc68ce78e5f419af11ad4ff5db6b20864dd548bf
zetaloop <zetaloop@outlook.com>
2024-12-25 19:50:31 +0800

    [frontend] Fix build path

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: '../backend/src/main/resources/static',
   basePath: '',
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
