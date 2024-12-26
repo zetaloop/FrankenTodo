ab6899889e7a6d2877c9dce2044be351168081a5
zetaloop <zetaloop@outlook.com>
2024-12-27 06:19:30 +0800

    [backend] Fix projects error

--- backend/src/main/java/build/loop/todo/repository/ProjectMemberRepository.java
+++ backend/src/main/java/build/loop/todo/repository/ProjectMemberRepository.java
@@ -4,6 +4,7 @@ import build.loop.todo.model.entity.Project;
 import build.loop.todo.model.entity.ProjectMember;
 import build.loop.todo.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,8 @@ import java.util.Optional;
 
 @Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, String> {
+    
+    @Query("SELECT pm FROM ProjectMember pm JOIN FETCH pm.project WHERE pm.user = :user")
     List<ProjectMember> findByUser(User user);
     
     List<ProjectMember> findByProject(Project project);
