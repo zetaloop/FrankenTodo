75076f8e22e5cfcfd3917e566e46a04004994660
zetaloop <zetaloop@outlook.com>
2024-12-30 01:20:11 +0800

    [frontend] Update api index

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,10 +1,5 @@
 export * from './types'
 export * from './client'
-export * from './auth'
-export * from './projects'
-export * from './tasks'
-export * from './labels'
-export * from './user'
 
 // 导出统一的 API 对象
 import { authApi } from './auth'
@@ -12,6 +7,13 @@ import { projectsApi } from './projects'
 import { tasksApi } from './tasks'
 import { labelsApi } from './labels'
 import { userApi } from './user'
+
+// 导出统一的 Mock API 对象
+// import { authApiMock as authApi } from './auth-mock'
+// import { projectsApiMock as projectsApi } from './projects-mock'
+// import { tasksApiMock as tasksApi } from './tasks-mock'
+// import { labelsApiMock as labelsApi } from './labels-mock'
+// import { userApiMock as userApi } from './user-mock'
 
 export { authApi, projectsApi, tasksApi, labelsApi, userApi }
 
